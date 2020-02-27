f0947b269b0daa2d243991a8b3da14ba346ed6cf
drygnet <github@dryg.net>
2020-02-27 21:06:01 +0000

    Deploying to gh-pages from  - e1a0a1c0a91b23a810570ff5c049890ffb516b05 🚀

--- build/p-8dcbbfa4.system.js
+++ build/p-8dcbbfa4.system.js
@@ -1,1 +1,1 @@
-System.register(["./p-3e2b23ef.system.js"],(function(){"use strict";var t,e;return{setters:[function(n){t=n.p;e=n.b}],execute:function(){t().then((function(t){return e([["p-gjjxfhrn.system",[[1,"my-component",{first:[1],middle:[1],last:[1]}]]]],t)}))}}}));
+System.register(["./p-3e2b23ef.system.js"],(function(){"use strict";var e,t;return{setters:[function(n){e=n.p;t=n.b}],execute:function(){e().then((function(e){return t([["p-hnqeef6x.system",[[1,"my-component",{first:[1],middle:[1],last:[1]}]]]],e)}))}}}));
