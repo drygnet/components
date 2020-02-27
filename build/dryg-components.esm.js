4371dd0eaf8d2013ff016651ed1137e2482549e0
drygnet <github@dryg.net>
2020-02-27 20:14:04 +0000

    Deploying to gh-pages from  - fd8a1fc2e3f52872b89bf9d4fa23232e0eb61a98 🚀

---
+++ build/dryg-components.esm.js
@@ -0,0 +1,1 @@
+import{p as m,b as p}from"./p-391df9eb.js";m().then(m=>p([["p-9thhigyy",[[1,"my-component",{first:[1],middle:[1],last:[1]}]]]],m));
---
+++ build/p-8dcbbfa4.system.js
@@ -0,0 +1,1 @@
+System.register(["./p-3e2b23ef.system.js"],(function(){"use strict";var t,e;return{setters:[function(n){t=n.p;e=n.b}],execute:function(){t().then((function(t){return e([["p-gjjxfhrn.system",[[1,"my-component",{first:[1],middle:[1],last:[1]}]]]],t)}))}}}));
